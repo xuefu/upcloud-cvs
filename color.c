f1d638bb5f6893e384faba76ec7a901105f877ed
xuefu <xuefu.sh@gmail.com>
2014-05-17 23:40:16 +0800

    add color

---
+++ color.c
@@ -0,0 +1,21 @@
+#include "color.h"
+
+void fg_red_color()
+{
+    printf("\033[0m\033[31m");
+}
+
+void fg_blue_color()
+{
+    printf("\033[0m\033[34m");
+}
+
+void fg_green_color()
+{
+    printf("\033[0m\033[32m");
+}
+
+void reset_color()
+{
+    printf("\033[0m");
+}
---
+++ color.h
@@ -0,0 +1,18 @@
+#ifndef _COLOR_H
+#define _COLOR_H
+
+#include <stdio.h>
+
+/* set red color */
+void fg_red_color();
+
+/* set blue color */
+void fg_blue_color();
+
+/* set green color */
+void fg_green_color();
+
+/* reset origin color */
+void reset_color();
+    
+#endif
